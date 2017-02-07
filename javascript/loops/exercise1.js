691452472ab9501cff9586664bbdaaae72e1334b
jenhaga <jenhaga14@gmail.com>
2017-02-06 16:06:09 -0800

    Exercise One
    Condensed repetitive code with a for loop to create 10 divs

--- javascript/loops/exercise1.js
+++ javascript/loops/exercise1.js
@@ -1,6 +1,6 @@
 var exerciseOneBody = document.getElementById("exercise-1-body");
 
-var divOne = document.createElement("div");
+/*var divOne = document.createElement("div");
 divOne.innerHTML = "This is div 1";
 exerciseOneBody.appendChild(divOne);
 
@@ -14,4 +14,11 @@ exerciseOneBody.appendChild(divThree);
 
 var divFour = document.createElement("div");
 divFour.innerHTML = "This is div 4";
-exerciseOneBody.appendChild(divFour);
+exerciseOneBody.appendChild(divFour);*/
+
+var i
+var div = document.createElement("div");
+for (var i = 1; i < 11; i ++) {
+  div.innerHTML += "<div>This is div" + i + "</div";
+  exerciseOneBody.appendChild(div);
+}
