46fa907422f4e0b07e543904a0c1dfa2866ba84a
jenhaga <jenhaga14@gmail.com>
2017-02-06 23:15:09 -0800

    Exercise 2: SuperStars, best attempt
    I have the right number of parenthesis but not the right number of
    stars. There is only one star in each.

--- javascript/loops/exercise2.js
+++ javascript/loops/exercise2.js
@@ -34,7 +34,10 @@ function stars() {
 
 function superStars() {
   var N = parseInt(document.getElementById("N").value);
-
+  var result = " ";
+  for (var i = 1; i <= N; i ++) {
+    result += "(*)";
+  }
   document.getElementById("answer").innerHTML = result;
 }
 
