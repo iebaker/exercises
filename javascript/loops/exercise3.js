53bbda0e22325a348f24bdd967e56a8b23b46796
jenhaga <jenhaga14@gmail.com>
2017-02-08 14:29:30 -0800

    retry

--- javascript/loops/exercise3.js
+++ javascript/loops/exercise3.js
@@ -7,15 +7,18 @@ function playNumberGuess() {
     var high = parseInt(document.getElementById("range-high").value);
     var answer = makeRandomNumber(low,high);
     var guess = prompt("Guess a number between the two you chose.");
-    if (guess === answer) {
-      alert("You win!")
-    } else if (guess > answer) {
-      alert("Your guess is too high");
-      guess = prompt("Guess again.");
-    } else if (guess < answer) {
-      alert("Your guess is too low.")
-      guess = prompt("Guess again.");
-    }
+    var i = 0;
+    while (guess !== answer) {
+       if (guess > answer) {
+        alert("Your guess is too high");
+        guess = prompt("Guess again.");
+      } else if (guess < answer) {
+        alert("Your guess is too low.")
+        guess = prompt("Guess again.");
+      }  else if (guess === answer) {
+          alert("You win!")
+      }
+   }
 }
 
 function makeRandomNumber (low, high) {
